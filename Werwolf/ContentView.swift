3c0cd5ddf0b2bddd53dd129b9cd60760b3fe3843
Carl Steen <carldodo@mail.de>
2025-07-20 16:51:21 +0200

    Initial Commit

---
+++ Werwolf/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Werwolf
+//
+//  Created by Carl on 20.07.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Werwolf/WerwolfApp.swift
@@ -0,0 +1,17 @@
+//
+//  WerwolfApp.swift
+//  Werwolf
+//
+//  Created by Carl on 20.07.25.
+//
+
+import SwiftUI
+
+@main
+struct WerwolfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
